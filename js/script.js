d58b7b122a424a426326599c6c45e1aa83c97a0d
dineshkrishdev <dinesh@dineshkrish.com>
2017-12-17 18:14:53 +0530

    files are added

--- js/script.js
+++ js/script.js
@@ -1,89 +1,75 @@
-var db = new Dexie("spellryt");
+var schemaBuilder = lf.schema.create('spellryt', 1);
 
-db.version(1).stores({
-    a_series: 'word, status',
-    b_series: 'word, status',
-    c_series: 'word, status',
-    d_series: 'word, status',
-    e_series: 'word, status',
-    f_series: 'word, status',
-    g_series: 'word, status',
-    h_series: 'word, status',
-    i_series: 'word, status',
-    j_series: 'word, status',
-    k_series: 'word, status',
-    l_series: 'word, status',
-    m_series: 'word, status',
-    n_series: 'word, status',
-    o_series: 'word, status',
-    p_series: 'word, status',
-    q_series: 'word, status',
-    r_series: 'word, status',
-    s_series: 'word, status',
-    t_series: 'word, status',
-    u_series: 'word, status',
-    v_series: 'word, status',
-    w_series: 'word, status',
-    x_series: 'word, status',
-    y_series: 'word, status',
-    z_series: 'word, status'
-});
+var spellryt;
 
 app.controller('prepare', function($location, $http) {
 
     var selected = $location.search().data;
 
-    if(selected != undefined) {
+    schemaBuilder.createTable('a_series').
+    addColumn('id', lf.Type.INTEGER).
+    addColumn('word', lf.Type.STRING).
+    addColumn('status', lf.Type.BOOLEAN).
+    addPrimaryKey(['id']).
+    addIndex('idxWord', ['word'], false, lf.Order.DESC);
+    
+    var word;
 
-        $http.get("http://localhost:9005/resources/"+selected+".json")
+    schemaBuilder.connect().then(function(db) {
+        spellryt = db;
+        
+        word = db.getSchema().table('a_series');
+
+        $http.get("/resources/a.json")
         .then(function(response) {
-
             var words = response.data.words;
 
-                
+            var count = 1;
 
-                words.forEach(element => {
-                    
-                    var obj = new Object();
+            words.forEach(element => {
 
-                    obj.word = element;
-                    obj.status = "N";
-                    
+                var obj = new Object();
+                obj.id = count;
+                obj.word = element;
+                obj.status = false;
 
-                    db.table("a_series").put(obj).then (function(){
-                        return obj;
-                    }).catch(function(error) {
-                    alert ("Ooops: " + error);
-                    });
-                    
-                    
-
-                });
+                var row = word.createRow(obj);
+                db.insertOrReplace().into(word).values([row]).exec();
+                count ++;
+            });
 
         });
+      });
 
-        
-    } else {
-
-
-    }
-
-    var wordList = new Array();
-
-    db.table("a_series")
-    .where('status')
-    .equals('N').each(function(x) {
-
-        var temp = new Object();
-
-        temp.word = x.word;
-        temp.status = x.status;
-
-        wordList.push(temp);
-
-        console.log(temp);
-    });        
-
-    
 
 });
+
+var current;
+
+function getData() {
+
+    var table = spellryt.getSchema().table("a_series");
+
+    var test = spellryt.select().
+    from(table).
+    where(table.status.eq(false)).
+    exec().then(function(result) {
+
+        current = result[(Math.floor(Math.random() * result.length))];
+
+        console.log(current);
+        
+    });
+}
+
+function update() {
+
+    var table = spellryt.getSchema().table("a_series");
+    
+    spellryt.update(table).
+    set(table.status, true).
+    where(table.id.eq(current.id)).
+    exec();
+
+    console.log("updated");
+}
